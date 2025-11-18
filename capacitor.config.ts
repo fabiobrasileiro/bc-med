201429a7cb73a36197506c44222e079b8e740e70
Fábio Brasileiro <brasileirowinner@gmail.com>
2025-11-18 11:19:01 -0300

    capacitor install

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bc.medmanager',
+  appName: 'BCMedManager',
+  webDir: 'build'
+};
+
+export default config;
